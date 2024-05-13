0c82a3f030afd310dc58e521949bb80d1b5467c1
EvaCampbell19 <campbelleva46@gmail.com>
2024-05-13 18:21:10 +0530

    Customer Review section design

--- src/app/(landing)/product/2A-AF-White/page.tsx
+++ src/app/(landing)/product/2A-AF-White/page.tsx
@@ -100,18 +100,17 @@ export default function One() {
   const data = ["/2A/2AAfWhite.jpg", "/2A/2AAfWhiteB.jpg"];
 
   const breadcrumbItems = [
-    { href: '/', label: 'Home' },
-    { href: '/stores', label: 'Store' },
-    { label: '2A AF White' } // This item does not have a link
+    { href: "/", label: "Home" },
+    { href: "/stores", label: "Store" },
+    { label: "2A AF White" }, // This item does not have a link
   ];
-
 
   return (
     <>
-    <Breadcrumb items={breadcrumbItems} />
+      <Breadcrumb items={breadcrumbItems} />
       <main
         style={{
-          backgroundColor: "#171717",
+          // backgroundColor: "#171717",
           fontFamily: "'Inter',sans-serif",
         }}
         className="grid h-screen justify-center"
@@ -170,6 +169,200 @@ export default function One() {
           </div>
         </div>
       </main>
+
+      <div className="m-8 lg:flex-row border border-gray-200 bg-white p-6 dark:border-gray-700 dark:bg-gray-800 md:space-x-6">
+    <div className="flex flex-col md:flex-row md:space-x-2 md:space-y-0 m-6">
+
+        <button type="button" className="mb-2 md:mb-0 border-gray-200 bg-[#F5F5F5] px-5 py-2.5 text-sm font-medium text-[#121212] hover:bg-[#121212] hover:text-white focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700 lg:h-[54px] lg:w-[290px]">
+            Description
+        </button>
+        <button type="button" className="mb-2 md:mb-0 border-gray-200 bg-[#F5F5F5] px-5 py-2.5 text-sm font-medium text-[#121212] hover:bg-[#121212] hover:text-white focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700 lg:h-[54px] lg:w-[290px]">
+            Size Charts
+        </button>
+        <button type="button" className="mb-2 md:mb-0 border-gray-200 bg-[#F5F5F5] px-5 py-2.5 text-sm font-medium text-[#121212] hover:bg-[#121212] hover:text-white focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700 lg:h-[54px] lg:w-[290px]">
+            Shipping Policy
+        </button>
+        <button type="button" className="mb-2 md:mb-0 border-gray-200 bg-[#F5F5F5] px-5 py-2.5 text-sm font-medium text-[#121212] hover:bg-[#121212] hover:text-white focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700 lg:h-[54px] lg:w-[290px]">
+            Return Policy
+        </button>
+    </div>
+    <p className="text-sm mt-4 md:mt-0 mb-4 md:mb-4 md:text-base lg:text-lg xl:text-lg font-normal text-gray-700 dark:text-gray-400 leading-normal">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae tincidunt augue, at porta sapien. Suspendisse ac finibus tellus, non dapibus nibh. Nulla aliquam, odio ultricies aliquet pretium, lectus velit lacinia diam, dignissim egestas tortor nisi sagittis nunc. Sed vehicula neque tortor, eget finibus arcu molestie nec. Sed rutrum nisi porta turpis vehicula, facilisis finibus sapien tincidunt. Pellentesque sollicitudin purus quis dolor finibus congue.
+    </p>
+    <p className="text-sm mt-4 md:mt-0 mb-4 md:mb-4 md:text-base lg:text-lg xl:text-lg font-normal text-gray-700 dark:text-gray-400 leading-normal">
+        In hac habitasse platea dictumst. Pellentesque volutpat enim nec nibh vulputate lacinia. Donec tempor, sem id placerat faucibus, sapien urna convallis leo, nec bibendum sapien diam mattis nulla. Mauris auctor velit id dui lobortis rhoncus. Praesent eget mattis neque. Sed sollicitudin porttitor nisl. Nullam at nunc vulputate, sollicitudin ipsum sit amet, venenatis sapien. Maecenas imperdiet sem ut ante varius, a aliquet mauris tristique.
+    </p>
+    <p className="text-sm mt-4 md:mt-0 mb-4 md:mb-4 md:text-base lg:text-lg xl:text-lg font-normal text-gray-700 dark:text-gray-400 leading-normal">
+        Mauris at elementum elit, eget feugiat elit. Pellentesque vel porta velit, quis tristique justo. In lobortis vehicula odio, non pretium felis. Donec tempus justo rhoncus augue malesuada, et aliquet nisi aliquet. Proin vulputate odio tortor, et pulvinar erat interdum quis. Morbi finibus fringilla quam vel finibus. Fusce vel blandit lectus, et tincidunt sapien. Sed facilisis erat in orci volutpat sodales.
+    </p>
+</div>
+<section className="py-24 relative">
+        <div className="w-full max-w-7xl px-4 md:px-5 lg-6 mx-auto">
+        <div className="flex items-center justify-center md:space-x-2 mb-11">
+  <div className="flex-grow border-t border-black"></div>
+  <h2 className="font-manrope font-bold text-6xl text-black text-center mb-2">Customer Reviews</h2>
+  <div className="flex-grow border-t border-black"></div>
+
+</div>    
+            <div className="grid grid-cols-1 gap-8">
+
+                <div className="grid grid-cols-12 max-w-sm sm:max-w-full mx-auto">
+                    <div className="col-span-12 lg:col-span-10 ">
+                       
+                        <div className="sm:flex gap-28">
+                          <div
+                        className="col-span-12 lg:col-span-2 max-lg:hidden flex lg:items-center flex-row lg:flex-col justify-center max-lg:pt-6 ">
+                        <div className="flex items-center gap-2 lg:justify-between w-full mb-5">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30"
+                                fill="none">
+                                <g clip-path="url(#clip0_13624_2090)">
+                                    <path
+                                        d="M14.1033 2.56698C14.4701 1.82374 15.5299 1.82374 15.8967 2.56699L19.1757 9.21093C19.3214 9.50607 19.6029 9.71064 19.9287 9.75797L27.2607 10.8234C28.0809 10.9426 28.4084 11.9505 27.8149 12.5291L22.5094 17.7007C22.2737 17.9304 22.1662 18.2614 22.2218 18.5858L23.4743 25.8882C23.6144 26.7051 22.7569 27.3281 22.0233 26.9424L15.4653 23.4946C15.174 23.3415 14.826 23.3415 14.5347 23.4946L7.9767 26.9424C7.24307 27.3281 6.38563 26.7051 6.52574 25.8882L7.7782 18.5858C7.83384 18.2614 7.72629 17.9304 7.49061 17.7007L2.1851 12.5291C1.59159 11.9505 1.91909 10.9426 2.73931 10.8234L10.0713 9.75797C10.3971 9.71064 10.6786 9.50607 10.8243 9.21093L14.1033 2.56698Z"
+                                        fill="#FBBF24" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_13624_2090">
+                                        <rect width="30" height="30" fill="white" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30"
+                                fill="none">
+                                <g clip-path="url(#clip0_13624_2090)">
+                                    <path
+                                        d="M14.1033 2.56698C14.4701 1.82374 15.5299 1.82374 15.8967 2.56699L19.1757 9.21093C19.3214 9.50607 19.6029 9.71064 19.9287 9.75797L27.2607 10.8234C28.0809 10.9426 28.4084 11.9505 27.8149 12.5291L22.5094 17.7007C22.2737 17.9304 22.1662 18.2614 22.2218 18.5858L23.4743 25.8882C23.6144 26.7051 22.7569 27.3281 22.0233 26.9424L15.4653 23.4946C15.174 23.3415 14.826 23.3415 14.5347 23.4946L7.9767 26.9424C7.24307 27.3281 6.38563 26.7051 6.52574 25.8882L7.7782 18.5858C7.83384 18.2614 7.72629 17.9304 7.49061 17.7007L2.1851 12.5291C1.59159 11.9505 1.91909 10.9426 2.73931 10.8234L10.0713 9.75797C10.3971 9.71064 10.6786 9.50607 10.8243 9.21093L14.1033 2.56698Z"
+                                        fill="#FBBF24" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_13624_2090">
+                                        <rect width="30" height="30" fill="white" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30"
+                                fill="none">
+                                <g clip-path="url(#clip0_13624_2090)">
+                                    <path
+                                        d="M14.1033 2.56698C14.4701 1.82374 15.5299 1.82374 15.8967 2.56699L19.1757 9.21093C19.3214 9.50607 19.6029 9.71064 19.9287 9.75797L27.2607 10.8234C28.0809 10.9426 28.4084 11.9505 27.8149 12.5291L22.5094 17.7007C22.2737 17.9304 22.1662 18.2614 22.2218 18.5858L23.4743 25.8882C23.6144 26.7051 22.7569 27.3281 22.0233 26.9424L15.4653 23.4946C15.174 23.3415 14.826 23.3415 14.5347 23.4946L7.9767 26.9424C7.24307 27.3281 6.38563 26.7051 6.52574 25.8882L7.7782 18.5858C7.83384 18.2614 7.72629 17.9304 7.49061 17.7007L2.1851 12.5291C1.59159 11.9505 1.91909 10.9426 2.73931 10.8234L10.0713 9.75797C10.3971 9.71064 10.6786 9.50607 10.8243 9.21093L14.1033 2.56698Z"
+                                        fill="#FBBF24" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_13624_2090">
+                                        <rect width="30" height="30" fill="white" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30"
+                                fill="none">
+                                <g clip-path="url(#clip0_13624_2090)">
+                                    <path
+                                        d="M14.1033 2.56698C14.4701 1.82374 15.5299 1.82374 15.8967 2.56699L19.1757 9.21093C19.3214 9.50607 19.6029 9.71064 19.9287 9.75797L27.2607 10.8234C28.0809 10.9426 28.4084 11.9505 27.8149 12.5291L22.5094 17.7007C22.2737 17.9304 22.1662 18.2614 22.2218 18.5858L23.4743 25.8882C23.6144 26.7051 22.7569 27.3281 22.0233 26.9424L15.4653 23.4946C15.174 23.3415 14.826 23.3415 14.5347 23.4946L7.9767 26.9424C7.24307 27.3281 6.38563 26.7051 6.52574 25.8882L7.7782 18.5858C7.83384 18.2614 7.72629 17.9304 7.49061 17.7007L2.1851 12.5291C1.59159 11.9505 1.91909 10.9426 2.73931 10.8234L10.0713 9.75797C10.3971 9.71064 10.6786 9.50607 10.8243 9.21093L14.1033 2.56698Z"
+                                        fill="#FBBF24" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_13624_2090">
+                                        <rect width="30" height="30" fill="white" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30"
+                                fill="none">
+                                <g clip-path="url(#clip0_13624_2090)">
+                                    <path
+                                        d="M14.1033 2.56698C14.4701 1.82374 15.5299 1.82374 15.8967 2.56699L19.1757 9.21093C19.3214 9.50607 19.6029 9.71064 19.9287 9.75797L27.2607 10.8234C28.0809 10.9426 28.4084 11.9505 27.8149 12.5291L22.5094 17.7007C22.2737 17.9304 22.1662 18.2614 22.2218 18.5858L23.4743 25.8882C23.6144 26.7051 22.7569 27.3281 22.0233 26.9424L15.4653 23.4946C15.174 23.3415 14.826 23.3415 14.5347 23.4946L7.9767 26.9424C7.24307 27.3281 6.38563 26.7051 6.52574 25.8882L7.7782 18.5858C7.83384 18.2614 7.72629 17.9304 7.49061 17.7007L2.1851 12.5291C1.59159 11.9505 1.91909 10.9426 2.73931 10.8234L10.0713 9.75797C10.3971 9.71064 10.6786 9.50607 10.8243 9.21093L14.1033 2.56698Z"
+                                        fill="#FBBF24" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_13624_2090">
+                                        <rect width="30" height="30" fill="white" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                        </div>
+                        <p className="font-medium  flex text-lg leading-8 text-gray-400 lg:text-center whitespace-nowrap">
+                          <button className="mb-2 md:mb-0 border-gray-200 bg-[#121212] px-5 py-2 text-sm font-medium  text-white
+                          hover:bg-[#F5F5F5] hover:text-[#121212] focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:bg-gray-800 
+                          dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700 ">Verified buyer</button>
+                           <p className="font-medium text-lg leading-8 text-gray-900 pl-2 mb-2">Robert </p>
+                        </p>
+                   
+                    </div>
+                    
+                   
+                            <div className="text md:ml-12">
+                                <p className="font-medium text-lg leading-8 text-gray-900 mb-2">Robert Karmazov</p>
+                               
+                                <p className="font-normal text-base leading-7 text-gray-400 mb-4 lg:pr-8">One of the standout features of Pagedone is its intuitive and user-friendly interface. Navigating through the system feels natural, and the layout makes it easy to locate and utilize various design elements. This is particularly beneficial for designers looking to streamline their workflow.  </p>
+                               
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+                
+                <div className="pb-8 border-b border-gray-100 w-full"></div>
+                
+  <div className="flex justify-center items-center gap-2">
+  <button
+    className="flex items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-full select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+    type="button">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+      aria-hidden="true" className="w-4 h-4">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"></path>
+    </svg>
+  </button>
+    <button
+      className="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-full text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button">
+      <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+        1
+      </span>
+    </button>
+    <button
+      className="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-full text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button">
+      <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+        2
+      </span>
+    </button>
+    <button
+      className="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-full bg-gray-900 text-center align-middle font-sans text-xs font-medium uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button">
+      <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+        3
+      </span>
+    </button>
+    <button
+      className="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-full text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button">
+      <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+        4
+      </span>
+    </button>
+    <button
+      className="relative h-10 max-h-[40px] w-10 max-w-[40px] select-none rounded-full text-center align-middle font-sans text-xs font-medium uppercase text-gray-900 transition-all hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="button">
+      <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+        5
+      </span>
+    </button>
+    <button
+    className="flex  items-center gap-2 px-6 py-3 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-full select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+    type="button">
+   
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+      aria-hidden="true" className="w-4 h-4">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"></path>
+    </svg>
+  </button>
+  </div>       
+        </div>
+        </div>
+        <div className="flex items-center gap-4">
+</div> 
+</section>
+                                            
+
     </>
   );
 }
